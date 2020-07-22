591d3f660f3d3df8e650d425265ba59547d4ff9c
Ismael Perez <ismaelperezemail@gmail.com>
2020-07-22 16:37:31 -0700

    chore(Index): change .then to .catch

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post("/register", (req, res) => {
         .then(user => {
             res.json(user[0]);
         })
-        .then(err => res.status(400).json(err));
+        .catch(err => res.status(400).json(err));
 });
 
 app.get("/profile/:id", (req, res) => {
