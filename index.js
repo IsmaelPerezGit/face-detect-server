f641fc5faedfb1d16f9577a6eee4db869bd2a983
Ismael Perez <ismaelperezemail@gmail.com>
2020-07-24 10:38:30 -0700

    chore(Index): changed knex setting

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const pg = knex({
     client: "pg",
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        ssl: true,
     },
 });
 
